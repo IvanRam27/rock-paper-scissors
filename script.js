bf1d8305e95e077cd2a4b2a45e25eb9e6d0cbbdf
Ivan <ivanmr2796@gmail.com>
2022-11-03 20:22:59 -0700

    add game function to play multiple rounds

--- script.js
+++ script.js
@@ -4,8 +4,11 @@
 //compare user's and computer's choice and define who wins
 //tell the user what was the computer's choice and who won
 
-let computerSelection = getComputerChoice()
-let playerSelection = prompt("Welcome to Rock, Paper & Scissors!", "Choose")
+let computerSelection
+let playerSelection 
+let playerScore = 0
+let computerScore = 0
+let scoreMessage
 
 function getComputerChoice() {
     let randomNumber = Math.random();
@@ -24,33 +27,66 @@ function playRound (playerSelection, computerSelection) {
     if (playerSelection === "rock" || playerSelection === "paper" || playerSelection === "scissors") {
 
     if (playerSelection === computerSelection)
-    return ("It's a tie!")
+    return ("It's a Tie!")
 
     if (playerSelection == "rock" && computerSelection == "scissors")
-    return("You Win! Rock beats Scissors")
+    return("You Win! Rock beats Scissors.")
 
     if (playerSelection == "rock" && computerSelection == "paper")
-    return("You Lose! Paper beats Rock")
+    return("You Lose! Paper beats Rock.")
 
     if (playerSelection == "paper" && computerSelection == "rock")
-    return("You Win! Paper beats Rock")
+    return("You Win! Paper beats Rock.")
 
     if (playerSelection == "paper" && computerSelection == "scissors")
-    return("You Lose! Scissors beats Paper")
+    return("You Lose! Scissors beats Paper.")
 
     if (playerSelection == "scissors" && computerSelection == "paper")
-    return("You Win! Scissors beats Paper")
+    return("You Win! Scissors beats Paper.")
 
     if (playerSelection == "scissors" && computerSelection == "rock")
-    return("You Lose! Rock beats Scissors")
+    return("You Lose! Rock beats Scissors.")
 
 
     }
 
-    return ("Please enter rock, paper or scissors")
+    return ("Please enter rock, paper or scissors.")
    
 }
 
-console.log(computerSelection)
-console.log(playRound(playerSelection,computerSelection))
+function game() {
+    for(let i = 1; i <= 5; i++){
+        playerSelection = prompt("Welcome to Rock, Paper & Scissors!", "Choose")
+        computerSelection = getComputerChoice()
+ 	    scoreMessage =playRound(playerSelection, computerSelection)
+  
+        if(scoreMessage.substr(4,3) === "Win") {
+            playerScore += 1
+            console.log("Round " + i + ": " + scoreMessage + " Score is " + playerScore + "-" + computerScore)
+        }
+  
+        if(scoreMessage.substr(4,4) === "Lose") {
+            computerScore += 1
+            console.log("Round " + i  + ": " + scoreMessage + " Score is " + playerScore + "-" + computerScore)
+        }
+  
+        if(scoreMessage.substr(7,3) === "Tie") {
+            console.log("Round " + i  + ": " + scoreMessage + " Score is " + playerScore + "-" + computerScore)
+        }
+	
+        if(scoreMessage.substr(4,1) === "s") {
+            console.log(scoreMessage)
+            i -= 1
+        }
+    }
+    if (playerScore > computerScore)
+    return "You Win the Game!"
+ 
+    if(computerScore > playerScore)
+    return "You Lose the Game!"
+ 
+    return "The Game is a tie!"
+ 
+}
 
+console.log(game())
